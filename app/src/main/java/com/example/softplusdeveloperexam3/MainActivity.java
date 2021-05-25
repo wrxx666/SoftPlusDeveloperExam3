8b9bb00fc7d8c2709fe5892b18c7f3a71c574d0b
wrxx666 <70403604+wrxx666@users.noreply.github.com>
2021-05-26 02:56:10 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/softplusdeveloperexam3/MainActivity.java
+++ app/src/main/java/com/example/softplusdeveloperexam3/MainActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
