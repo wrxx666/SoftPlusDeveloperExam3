91c79f9e13dd4996e4f31ada28e20c6ba18d55cf
wrxx666 <70403604+wrxx666@users.noreply.github.com>
2021-05-26 17:09:44 +0500

    Working app with an entrance counter(final with logs)

--- app/src/main/java/com/example/softplusdeveloperexam3/MainActivity.java
+++ app/src/main/java/com/example/softplusdeveloperexam3/MainActivity.java
@@ -6,17 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String APP_PREFERENCES = "mysettings";
     public static final String APP_PREFERENCES_ENTRANCE_COUNT = "entranceCount";
+    private final String LOG_TAG = "devExam3";
     Integer mIncrementedCounter = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
+            Log.d(LOG_TAG, "Cold boot detected");
             SharedPreferences mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
             int entranceCount = mSettings.getInt(APP_PREFERENCES_ENTRANCE_COUNT, 0);
             if (entranceCount == 2) {
@@ -30,14 +33,20 @@ public class MainActivity extends AppCompatActivity {
             editor.putInt(APP_PREFERENCES_ENTRANCE_COUNT, entranceCount);
             editor.apply();
             mIncrementedCounter = entranceCount;
+        } else {
+            Log.d(LOG_TAG, "Activity restored");
         }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+
         super.onSaveInstanceState(outState);
         if (mIncrementedCounter != null){
             outState.putInt("count",mIncrementedCounter);
+            Log.d(LOG_TAG, "current entrance count = "+ mIncrementedCounter);
+        } else {
+            Log.d(LOG_TAG, "Counter not stored");
         }
     }
 }
