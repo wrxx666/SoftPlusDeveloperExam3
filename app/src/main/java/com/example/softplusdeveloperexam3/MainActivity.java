415623a06d3f14f8d7ad33d8b76bacfc9e1cd456
wrxx666 <70403604+wrxx666@users.noreply.github.com>
2021-05-26 14:43:20 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/softplusdeveloperexam3/MainActivity.java
+++ app/src/main/java/com/example/softplusdeveloperexam3/MainActivity.java
@@ -10,19 +10,19 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     public static final String APP_PREFERENCES = "mysettings";
     public static final String APP_PREFERENCES_ENTRANCE_COUNT = "entranceCount";
-    SharedPreferences mSettings;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast toast = Toast.makeText(getApplicationContext(),
-                                "It's a third entrance",
-                                        Toast.LENGTH_LONG);
-        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = mSettings.edit();
         int entranceCount = mSettings.getInt(APP_PREFERENCES_ENTRANCE_COUNT,0);
         editor.apply();
         if (entranceCount == 2){
+            Toast toast = Toast.makeText(getApplicationContext(),
+                                "It's a third entrance",
+                                        Toast.LENGTH_LONG);
             toast.show();
         }
         entranceCount++;
