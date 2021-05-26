90c3ee6f88dfc856e7ee851e6f1d95f95c84b50f
wrxx666 <70403604+wrxx666@users.noreply.github.com>
2021-05-26 16:31:19 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/softplusdeveloperexam3/MainActivity.java
+++ app/src/main/java/com/example/softplusdeveloperexam3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.softplusdeveloperexam3;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -10,23 +11,33 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     public static final String APP_PREFERENCES = "mysettings";
     public static final String APP_PREFERENCES_ENTRANCE_COUNT = "entranceCount";
-    
+    Integer mIncrementedCounter = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = mSettings.edit();
-        int entranceCount = mSettings.getInt(APP_PREFERENCES_ENTRANCE_COUNT,0);
-        if (entranceCount == 2){
-            Toast toast = Toast.makeText(getApplicationContext(),
-                                "It's a third entrance",
-                                        Toast.LENGTH_LONG);
-            toast.show();
+        if (savedInstanceState == null) {
+            SharedPreferences mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = mSettings.edit();
+            int entranceCount = mSettings.getInt(APP_PREFERENCES_ENTRANCE_COUNT, 0);
+            if (entranceCount == 2) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "It's a third entrance",
+                        Toast.LENGTH_LONG);
+                toast.show();
+            }
+            entranceCount++;
+            editor.putInt(APP_PREFERENCES_ENTRANCE_COUNT, entranceCount);
+            editor.apply();
+            mIncrementedCounter = entranceCount;
         }
-        entranceCount++;
-        editor.putInt(APP_PREFERENCES_ENTRANCE_COUNT,entranceCount);
-        editor.apply();
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mIncrementedCounter != null){
+            outState.putInt("count",mIncrementedCounter);
+        }
     }
 }
